1a3e006227cd65075d0c08bf9dd103d726bbbc22
MLCanillas <mlcanillas12@gmail.com>
2023-09-06 14:16:25 +0800

    episode2

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-console.log("Testing!");
-console.log("Testing2");
+// A variable is a container for storing Data
+// A Variable behaves as if it was the value that it contains
 
-window.alert("Testing alert!"); 
+// Two steps:
+// 1. Declaration (var, let, const)
+// 2. Assignment (= assignment operator)
 
-//this is a comment
 
-/*
-This
-is
-a
-multiline
-comment
-*/
+
+
+let firstname = "Migz" //strings
+let age = 30; //numbers
+let student = false; //booleans
+
+console.log("Hello",firstname);
+console.log("You are",age,"Years old");
+console.log("Enrolled:",student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstname
+document.getElementById("p2").innerHTML = "You are " + age + " Years old"
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
