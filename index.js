99494ceefdc4e205f44f1ecced58a815128e1715
MLCanillas <mlcanillas12@gmail.com>
2023-09-07 12:09:33 +0800

    episode6

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-//TYpe Conversion = change the datatype of a value to another
-//                  (Strings, numbers, booleans)
+// const = a variable that can't be changed
+const PI = 3.14159
+let radius;
+let circumferrence;
 
-/*
-let age =window.prompt("How old are you? ");
-console.log(typeof age);
-age = Number(age);
-console.log(typeof age);
-age += 1;
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
 
-console.log("Happy Birthday! You are", age, "Years old");
-*/
+//PI = 420.69;
 
-let x;
-let y;
-let z;
+circumferrence = 2 * PI * radius;
 
-x = Number("3.14");
-y = String(3.14);
-z = Boolean("")
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+console.log("The Circumferrence is:", circumferrence);
