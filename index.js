9be8c3d90c0cbc6c2dc64a444e428886a642ddfe
MLCanillas <mlcanillas12@gmail.com>
2023-09-07 11:57:20 +0800

    episode5

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// how to accept user input
-//make sure to have <script src="index.js"></script> inside your html code
+//TYpe Conversion = change the datatype of a value to another
+//                  (Strings, numbers, booleans)
 
-//EASY WAY With a window prompt
+/*
+let age =window.prompt("How old are you? ");
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
 
-//let username = window.prompt("What's your name?");
-//console.log(username);
+console.log("Happy Birthday! You are", age, "Years old");
+*/
 
+let x;
+let y;
+let z;
 
-
-// DIFFICULT WAY HTML textbox
-
-
-let username;
-document.getElementById("myButton").onclick = function(){
- username = document.getElementById("myText").value
- console.log(username);
- document.getElementById("myLabel").innerHTML = "Hello " + username;
-}
-
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("")
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
